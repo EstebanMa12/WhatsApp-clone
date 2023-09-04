a92ca112cbeb041591be729eae6b72a64bb827a1
EstebanMa12 <desteban.maya@udea.edu.co>
2023-09-03 23:21:59 -0500

    Add Sign Up functionality and styles

--- src/login.js
+++ src/login.js
@@ -2,8 +2,15 @@
 // const root = document.querySelector('#root')
 // document.head.appendChild(Styles)
 import LoginTemplate from '../public/templates/Login.html'
+import SignUpTemplate from '../public/templates/Sign_up.html'
 
 document.querySelector('#root').innerHTML = LoginTemplate
+document.querySelector(".signup").onclick =() => {
+    document.querySelector('#root').innerHTML = SignUpTemplate
+    document.querySelector(".signup-form__button").onclick =() => {
+        document.querySelector('#root').innerHTML = LoginTemplate
+    }
+}
 
 console.log('HELLO FROM LOGIN')
 
